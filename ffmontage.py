273648ae8d4db1bb8bc2d8aac84052af1ed11f80
Shankar Mahadevan G <53359635+shankrmahadevan@users.noreply.github.com>
2020-10-27 19:46:19 +0530

    Update ffmontage.py

--- ffmontage.py
+++ ffmontage.py
@@ -100,7 +100,7 @@ class FFMontage:
                         start_time = current_time - datetime.timedelta(seconds=2)
                         process_str = f'ffmpeg -i {self.video_path} -ss {start_time.time()} -to {end_time.time()} -c copy -preset ultrafast {self.concat_dir}/{str(vid_no)}.mp4'
                         subprocess.run([process_str], shell=True)
-                        text_file.write(f'file {self.concat_dir}/{str(vid_no)}.mp4\n')
+                        text_file.write(f'file {self.concat_dir.lstrip('temp/')}/{str(vid_no)}.mp4\n')
                         bar.set_postfix_str(f'Partitions : {vid_no}')
                         vid_no += 1
                         current_time = end_time
@@ -123,7 +123,7 @@ class FFMontage:
         text_file.close()
         now = datetime.datetime.today().strftime("Montage_%D %H_%M_%S")
         concat_file_name = f'{now}.mp4'
-        ffmpeg_cmd = f"ffmpeg -f concat -safe 0 -i temp/text_file.txt -c copy " + concat_file_name
+        ffmpeg_cmd = f"ffmpeg -safe 0 -f concat -segment_time_metadata 1 -i temp/text_file.txt -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 -preset ultrafast -max_muxing_queue_size 9999 " + concat_file_name
         subprocess.run([ffmpeg_cmd], shell=True)
-#         shutil.rmtree('temp/')
+        shutil.rmtree('temp/')
         
