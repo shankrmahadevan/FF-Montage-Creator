47de4ecfdcff2b4fbf968a74d49262403e1b5b33
Shankar Mahadevan G <53359635+shankrmahadevan@users.noreply.github.com>
2020-10-28 14:36:49 +0530

    Update ffmontage.py

--- ffmontage.py
+++ ffmontage.py
@@ -118,90 +118,11 @@ class FFMontage:
           return
       bar.close()
       cap.release()
-      now = datetime.datetime.today().strftime("Montage_%D %H_%M_%S")
+      now = datetime.datetime.today().strftime("Montage_%H_%M_%S")
       concat_file_name = f'{now}.mp4'
-      video_files = [VideoFileClip(video) for video in sorted(glob.glob(self.concat_dir+'/**'), key=lambda x: int(x.split('/')[-1].split('.')[0]))]
-      final_clip = concatenate_videoclips(video_files)
-      final_clip.write_videofile(concat_file_name, verbose=False, progress_bar=False)
+      with open('txt1.txt', 'a') as text_file:
+          for file in sorted(glob.glob('temp/to_concat'+'/**'), key=lambda x: int(x.split('/')[-1].split('.')[0])):
+            text_file.write('file '+file+'\n')
+      subprocess.run(f'ffmpeg -f concat -safe 0 -i txt1.txt -c copy -preset ultrafast {concat_file_name}', shell=True)
       shutil.rmtree('temp/')
-
-#     def video_process(self):
-#         self.download_video()
-#         array = []
-#         done = 0 
-#         text_file = open('temp/text_file.txt', 'a')
-#         if not os.path.exists(self.concat_dir):
-#             os.mkdir(self.concat_dir)
-#         cap = cv2.VideoCapture(self.video_path)
-#         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-#         fps = cap.get(cv2.CAP_PROP_FPS)
-#         duration = total_frames / fps
-#         partition_cmds = []
-#         buffer_size = self.time_interval * fps
-
-#         current_frame_no = 0
-#         divisor = fps // 3
-#         seconds_per_frame = 1 / fps
-#         vid_no = 0
-#         bar = tqdm(total=total_frames)
-
-#         current_time = datetime.datetime(1, 2, 3, hour=0, minute=0, second=0)
-#         end_time = datetime.datetime(1, 2, 3, hour=0, minute=0, second=0)
-#         while cap.isOpened():
-#             try:
-#                 ret, frame = cap.read()
-#                 if not ret:
-#                     break
-#                 current_time += datetime.timedelta(seconds=seconds_per_frame)
-#                 if current_frame_no % divisor == 0:
-#                     if self.is_true(frame):
-#                         end_time = current_time + datetime.timedelta(seconds=self.time_interval)
-#                         i = 0
-#                         for img in array:
-#                             cv2.imwrite(f'temp/frames/{str(done)}.jpg', img)
-#                             done+=1
-#                         while cap.isOpened() and i < self.time_interval * fps:
-#                             ret, frame = cap.read()
-#                             if not ret:
-#                                 break
-#                             cv2.imwrite(f'temp/frames/{str(done)}.jpg', frame)
-#                             done+=1
-#                             current_frame_no += 1
-#                             bar.update(1)
-#                             i += 1
-#                         start_time = current_time - datetime.timedelta(seconds=self.time_interval)
-# #                         process_str = f'ffmpeg -i {self.video_path} -ss {start_time.time()} -to {end_time.time()} -c copy -preset ultrafast {self.concat_dir}/{str(vid_no)}.mp4'
-#                         process_str = f'ffmpeg -i {self.video_path} -ss {start_time.time()} -to {end_time.time()} -vn -q:a 0 -map a {self.concat_dir}/{str(vid_no)}.mp3'
-#                         subprocess.run([process_str], shell=True)
-#                         text_file.write(f'file toconcat/{str(vid_no)}.mp3\n')
-#                         bar.set_postfix_str(f'Partitions : {vid_no}')
-#                         vid_no += 1
-#                         current_time = end_time
-#                     else:
-#                         current_frame_no += 1
-#                         bar.update(1)
-#                 else:
-#                     array.append(frame)
-#                     if len(array)>buffer_size:
-#                         array = array[1:]
-#                     current_frame_no += 1
-#                     bar.update(1)
-
-#             except:
-#                 bar.close()
-#                 cap.release()
-#                 text_file.close()
-#                 shutil.rmtree('temp/')
-#                 print(sys.exc_info())
-#                 return
-#         bar.close()
-#         cap.release()
-#         text_file.close()
-#         now = datetime.datetime.today().strftime("Montage_%D %H_%M_%S")
-#         concat_file_name = f'{now}.mp4'
-#         subprocess.run([f'ffmpeg -i temp/frames/%d.jpg -c:v libx264 -pix_fmt yuv420p -crf 23 -preset ultrafast -y output.mp4 -async 1 -vsync 1'], shell=True)
-# #         ffmpeg_cmd = f"ffmpeg -safe 0 -f concat -segment_time_metadata 1 -i temp/text_file.txt -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 -preset ultrafast -max_muxing_queue_size 9999 " + concat_file_name
-# #         ffmpeg_cmd = f'ffmpeg -i temp/text_file.txt -acodec copy audio.mp3'
-# #         subprocess.run([ffmpeg_cmd], shell=True)
-# #         shutil.rmtree('temp/')
         
